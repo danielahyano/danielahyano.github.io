23bebf5edd06d97049356804e2319750224f1805
dyano <dyano@cern.ch>
2026-05-17 22:52:14 +0200

    deploy: build site into docs folder for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Build into a 'docs' folder instead of breaking the root
+  outDir: './docs',
+});
