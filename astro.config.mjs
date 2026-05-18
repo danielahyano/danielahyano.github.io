fb4906ba445153361c3da967ffc54d26c990ddaa
dyano <dyano@cern.ch>
2026-05-18 15:05:17 +0200

    fix: rename asset folder from _astro to assets to bypass jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Build into the docs folder for GitHub Pages
   outDir: './docs',
-  // Sets the base root path for a user page portfolio
   base: '/',
+  build: {
+    // Forces Astro to use a clean "assets" folder instead of "_astro"
+    assets: 'assets'
+  }
 });
